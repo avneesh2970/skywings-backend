2f1b84eb91d48f9e3e0f62a2e0c66ec575017d3a
cykoravish <ravishbisht85@gmail.com>
2025-05-09 17:14:41 +0530

    add get post user api

--- models/AdminUser.js
+++ models/AdminUser.js
@@ -1,35 +1,41 @@
-import mongoose from 'mongoose';
-import bcrypt from 'bcrypt';
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 
 const adminUserSchema = new mongoose.Schema({
   username: {
     type: String,
     required: true,
     unique: true,
-    trim: true
+    trim: true,
   },
   email: {
     type: String,
     required: true,
     unique: true,
     trim: true,
-    lowercase: true
+    lowercase: true,
   },
   password: {
     type: String,
-    required: true
+    required: true,
+  },
+  phone: {
+    type: String,
+  },
+  bio: {
+    type: String,
   },
   createdAt: {
     type: Date,
-    default: Date.now
-  }
+    default: Date.now,
+  },
 });
 
 // Pre-save hook to hash password before saving
 // adminUserSchema.pre('save', async function(next) {
 //   // Only hash the password if it's modified or new
 //   if (!this.isModified('password')) return next();
-  
+
 //   try {
 //     // Generate salt and hash password
 //     const salt = await bcrypt.genSalt(10);
@@ -45,6 +51,6 @@ const adminUserSchema = new mongoose.Schema({
 //   return await bcrypt.compare(candidatePassword, this.password);
 // };
 
-const AdminUser = mongoose.model('User', adminUserSchema);
+const AdminUser = mongoose.model("User", adminUserSchema);
 
 export default AdminUser;
