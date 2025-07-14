a8fb24d4751b93b6016852976ac19886c144b1df
cykoravish <ravishbisht85@gmail.com>
2025-07-14 18:30:35 +0530

    refactor express middleware to enable JSON parsing with increased limits

--- server.js
+++ server.js
@@ -51,7 +51,9 @@ app.use(
     credentials: true,
   })
 );
-app.use(express.json());
+// app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 // Token storage
